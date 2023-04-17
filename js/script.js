c6dadaf9994925886542bf515e21d624cb3187ca
WristeRR <abhishek.219302323@muj.manipal.edu>
2023-04-18 04:00:27 +0530

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const primaryHeader = document.querySelector(".primary-header");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const primaryNav = document.querySelector(".primary-navigation");
+
+navToggle.addEventListener("click", () => {
+  primaryNav.hasAttribute("data-visible")
+    ? navToggle.setAttribute("aria-expanded", false)
+    : navToggle.setAttribute("aria-expanded", true);
+  primaryNav.toggleAttribute("data-visible");
+  primaryHeader.toggleAttribute("data-overlay");
+
+  navToggle.firstElementChild.classList.contains("fa-bars")?
+  navToggle.firstElementChild.classList.replace("fa-bars","fa-xmark"):
+  navToggle.firstElementChild.classList.replace("fa-xmark","fa-bars");
+});
+  ScrollReveal({ 
+    reset: true,
+    distance:'60px',
+    duration:2200,
+    delay:200
+  });
+  ScrollReveal().reveal('.program-item',{delay:50,origin:'left'});
+  ScrollReveal().reveal('.container',{delay:50,origin:'bottom'});
